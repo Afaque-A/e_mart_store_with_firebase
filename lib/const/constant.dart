58c18de9f4fd8acdc538497a64227565089436b8
Afaque Ahmed <145118453+Afaque-A@users.noreply.github.com>
2024-08-18 17:08:10 +0500

    add velocity_x and get packages and also create some dart file

---
+++ lib/const/constant.dart
@@ -0,0 +1,5 @@
+export './colors.dart';
+export './images.dart';
+export './strings.dart';
+export './style.dart';
+export 'package:velocity_x/velocity_x.dart';
---
+++ lib/strings.dart
@@ -0,0 +1,4 @@
+
+const appname = "eMart";
+const appversion = "Version 1.0.0";
+const credits = "@Baaba Devs";
